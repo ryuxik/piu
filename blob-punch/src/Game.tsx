28ea2338f7ab8a066da98ddaa721de047123691e
Santiago Munoz <smunoz@mit.edu>
2019-12-03 23:53:52 -0800

    add starter with example of how to draw on canvas in react

---
+++ blob-punch/src/Game.tsx
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+
+type GameProps = {}
+type GameState = {
+
+}
+
+export class Game extends Component<GameProps, GameState> {
+	canvasRef : React.RefObject<HTMLCanvasElement>= React.createRef<HTMLCanvasElement>();
+
+	componentDidMount() {
+		this.draw();
+	}
+
+	private draw = () => {
+		const ctx = this.canvasRef.current!.getContext("2d"); 
+        ctx!.fillStyle = "green";
+        ctx!.beginPath();
+        ctx!.arc(50, 100,                        
+                20, 0, 2 * Math.PI);
+        ctx!.fill();
+        ctx!.stroke();
+	}
+
+    render() {
+    	return (
+	    <div> 
+	      <canvas ref={this.canvasRef} width={450} height={650} />
+	   	</div>
+    	);
+    }
+    
+    
+}
+
+export default Game;
